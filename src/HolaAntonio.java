ecebff48d934da53ba21837f2300d9702eac164f
Unai <unaipra2@gmail.com>
2025-11-11 11:31:41 +0100

    he cambiado el HolaAntonio

--- src/HolaAntonio.java
+++ src/HolaAntonio.java
@@ -1,5 +1,5 @@
 public class HolaAntonio {
      public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Antonio!");
+        System.out.println("Hola Antonio!");
     }
 }
